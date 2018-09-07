1d7a8009b9ac4f639723e436b2653efd8a693977
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-07 16:24:27 +0800

    bonus question answered to convert morse code to alphabet

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //Morning exercise: Convert strings into morse code.
 
-var morseCode = function(word){
+/*var morseCode = function(word){
     var splitWord = word.split(""); //["hello"]
-    var arr = [];
+    var alphabetToMorse = [];
     for (var i = 0; i < splitWord.length; i++){
-        /*
+
         for (var j = 0; j < Object.keys(alphabet).length; j++){
             if (splitWord[i] === Object.keys(alphabet)[j]){
-                arr.push(alphabet[Object.keys(alphabet)[j]]);
+                alphabetToMorse.push(alphabet[Object.keys(alphabet)[j]]);
             }
         }
-        */
+
         // splitWord[i] => ["h", "e", "l", "l", "o"]
         if (alphabet[ splitWord[i] ] !== undefined) {
-            arr.push(alphabet[splitWord[i]]);
+            alphabetToMorse.push(alphabet[splitWord[i]]);
         }
     }
-    console.log(arr);
-}
+    console.log(alphabetToMorse);
+}*/
 /*
 var firstWord = "hello world";
 morseCode(firstWord);
@@ -25,35 +25,60 @@ morseCode(firstWord);
 var secondWord = "we love javascript";
 morseCode(secondWord);
 */
+/*
 //further: Write a function that accepts a user input and be able to output the encoded value.
 var input = prompt("Insert your input: ");
 var inputLowCase = input.toLowerCase();
 if (typeof(inputLowCase) == "string"){
     morseCode(inputLowCase);
 }
-/*
+*/
 
 //Bonus: Decode
 var inp2 = prompt("Input your morse code: ");
-var realWords = function(word){
-    var arr2 =[];
-    for(var i = 0; i< word.length;i++){
-        if (word[i] === " "){
-            if word[i] ===
-        }
-        for(var j = 0; j< Object.keys(alphabet).length; j++){
-            if (word[i] === " "){
-                word[i].join()
-                if(word[i] === alphabet[Object.keys(alphabet)[j]]){
 
-                    arr2.push(Object.keys(alphabet)[j]);
-                 }
-            }
-
-        }
+var reverseObj = function(morse){
+    var newObj = {};
+    for(var key in morse){
+        newObj[morse[key]] = key;
     }
-    console.log(arr2);
+    return newObj;
 }
 
-realWords(inp2);
-*/
+//
+var decodeMorse = function(inp2, reverseObj){
+    var splitInput = inp2.split(" ");
+    var morseToAlphabet = [];
+    var revObj = reverseObj;
+    for (var i = 0; i< splitInput.length; i++){
+        if (revObj[ splitInput[i] ] !== undefined){
+            morseToAlphabet.push(revObj[ splitInput[i] ]);
+        }
+    }
+    console.log(morseToAlphabet);
+    return morseToAlphabet;
+ }
+decodeMorse(inp2,reverseObj(alphabet));
+
+// var realWords = function(word){
+//     var arr2 =[];
+//     for(var i = 0; i< word.length;i++){
+//         if (word[i] === " "){
+//             if word[i] ===
+//         }
+//         for(var j = 0; j< Object.keys(alphabet).length; j++){
+//             if (word[i] === " "){
+//                 word[i].join()
+//                 if(word[i] === alphabet[Object.keys(alphabet)[j]]){
+
+//                     arr2.push(Object.keys(alphabet)[j]);
+//                  }
+//             }
+
+//         }
+//     }
+//     console.log(arr2);
+// }
+
+//realWords(inp2);
+
