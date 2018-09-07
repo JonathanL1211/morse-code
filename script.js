4877e38b9d286d74108b3937cf22fa3f1b2847ea
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-07 13:11:21 +0800

    Took out else statement is further qn

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
 //Morning exercise: Convert strings into morse code.
 
-var morse_code = function(word){
-    var split_word = word.split("");
+var morseCode = function(word){
+    var splitWord = word.split(""); //["hello"]
     var arr = [];
-    for (var i = 0; i < split_word.length; i++){
+    for (var i = 0; i < splitWord.length; i++){
+        /*
         for (var j = 0; j < Object.keys(alphabet).length; j++){
-            if (split_word[i] === Object.keys(alphabet)[j]){
+            if (splitWord[i] === Object.keys(alphabet)[j]){
                 arr.push(alphabet[Object.keys(alphabet)[j]]);
             }
+        }
+        */
+        // splitWord[i] => ["h", "e", "l", "l", "o"]
+        if (alphabet[ splitWord[i] ] !== undefined) {
+            arr.push(alphabet[splitWord[i]]);
         }
     }
     console.log(arr);
 }
-/*var firstWord = "hello world";
-morse_code(firstWord);
+/*
+var firstWord = "hello world";
+morseCode(firstWord);
 
 var secondWord = "we love javascript";
-morse_code(secondWord);
+morseCode(secondWord);
 */
 //further: Write a function that accepts a user input and be able to output the encoded value.
 var input = prompt("Insert your input: ");
-var input_lowCase = input.toLowerCase();
-if (typeof(input_lowCase) == "string"){
-    morse_code(input_lowCase);
+var inputLowCase = input.toLowerCase();
+if (typeof(inputLowCase) == "string"){
+    morseCode(inputLowCase);
 }
-else {
-    console.log("Please input a string!");
+/*
+
+//Bonus: Decode
+var inp2 = prompt("Input your morse code: ");
+var realWords = function(word){
+    var arr2 =[];
+    for(var i = 0; i< word.length;i++){
+        if (word[i] === " "){
+            if word[i] ===
+        }
+        for(var j = 0; j< Object.keys(alphabet).length; j++){
+            if (word[i] === " "){
+                word[i].join()
+                if(word[i] === alphabet[Object.keys(alphabet)[j]]){
+
+                    arr2.push(Object.keys(alphabet)[j]);
+                 }
+            }
+
+        }
+    }
+    console.log(arr2);
 }
+
+realWords(inp2);
+*/
